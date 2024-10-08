baf356da1cf405e1e1297c948d2318712f8e7d03
Mukul Dave <mhdave@lbl.gov>
2024-10-08 12:23:19 -0700

    megabuild script cleanup initial

--- Build/megabuild.sh
+++ Build/megabuild.sh
@@ -2,25 +2,16 @@
 
 #set -o errexit
 
-mkdir test
-cd test
+#mkdir driver-build
+#cd driver-build
 export TOP=$(pwd)
 export TOP_MOD=$(pwd)/erf-amrwind-driver/Submodules
 
 #git clone git@github.com:jmsexton03/erf-amrwind-driver.git --recurse-submodules --shallow-submodules --branch splitting_build --single-branch # <folder>
-git clone git@github.com:jmsexton03/erf-amrwind-driver.git --branch splitting_build --single-branch # <folder>
+#git clone git@github.com:jmsexton03/erf-amrwind-driver.git --branch splitting_build --single-branch # <folder>
 
 cd ${TOP}/erf-amrwind-driver
 git submodule update --init --depth=1
-#cd ${TOP_MOD}/ERF
-#git submodule update --init
-
-#cd ${TOP_MOD}/amr-wind
-#git submodule update --init
-
-#cd ${TOP_MOD}
-#git clone ERF/Submodules/AMReX amrex
-#git clone amr-wind/submods/AMReX-Hydro
 
 cd ${TOP_MOD}/amrex
 cmake -DBUILD_SHARED_LIBS=ON \
@@ -44,11 +35,11 @@ make install
 
 
 cmake -DBUILD_SHARED_LIBS=ON \
+      -DCMAKE_SHARED_LINKER_FLAGS:STRING="-undefined dynamic_lookup" \
       -DCMAKE_INSTALL_PREFIX:PATH=${TOP}/ERF-install \
       -DCMAKE_CXX_COMPILER:STRING=mpicxx \
       -DCMAKE_C_COMPILER:STRING=mpicc \
       -DCMAKE_Fortran_COMPILER:STRING=mpifort \
-      -DMPIEXEC_PREFLAGS:STRING=--oversubscribe \
       -DCMAKE_BUILD_TYPE:STRING=Release \
       -DERF_DIM:STRING=3 \
       -DERF_ENABLE_MPI:BOOL=ON \
@@ -80,37 +71,10 @@ make -j16
 make install
 
 cd ${TOP}
-cmake -DCMAKE_INSTALL_PREFIX:PATH=${TOP}/erf-amrwind-driver-install-internal \
-      -DCMAKE_CXX_COMPILER:STRING=mpicxx \
-      -DCMAKE_C_COMPILER:STRING=mpicc \
-      -DCMAKE_Fortran_COMPILER:STRING=mpifort \
-      -DMPIEXEC_PREFLAGS:STRING=--oversubscribe \
-      -DCMAKE_BUILD_TYPE:STRING=Release \
-      -DAMRWIND_HOME:STRING=${TOP_MOD}/amr-wind \
-      -DERF_HOME:STRING=${TOP_MOD}/ERF \
-      -DERF_DIM:STRING=3 \
-      -DERF_ENABLE_MPI:BOOL=ON \
-      -DERF_ENABLE_TESTS:BOOL=OFF \
-      -DERF_ENABLE_FCOMPARE:BOOL=ON \
-      -DERF_ENABLE_DOCUMENTATION:BOOL=OFF \
-      -DERF_ENABLE_MULTIBLOCK:BOOL=ON \
-      -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON \
-      -DCMAKE_PREFIX_PATH="${TOP}/amrex-install/lib/cmake/AMReX;${TOP}/AMReX-Hydro-install/lib/cmake/AMReX-Hydro;${TOP}/amr-wind-install/lib/cmake/AMR-Wind" \
-      -DDRIVER_USE_INTERNAL_AMRWIND:BOOL=OFF \
-      -DDRIVER_USE_INTERNAL_ERF:BOOL=ON \
-      -DERF_USE_INTERNAL_AMREX:BOOL=OFF \
-      -DAMR_WIND_USE_INTERNAL_AMREX=OFF -DAMR_WIND_USE_INTERNAL_AMREX_HYDRO=OFF \
-      -S ${TOP}/erf-amrwind-driver -B ${TOP}/erf-amrwind-driver-build-internal
-cd ${TOP}/erf-amrwind-driver-build-internal
-make -j16
-make install
-
-cd ${TOP}
 cmake -DCMAKE_INSTALL_PREFIX:PATH=${TOP}/erf-amrwind-driver-install \
       -DCMAKE_CXX_COMPILER:STRING=mpicxx \
       -DCMAKE_C_COMPILER:STRING=mpicc \
       -DCMAKE_Fortran_COMPILER:STRING=mpifort \
-      -DMPIEXEC_PREFLAGS:STRING=--oversubscribe \
       -DCMAKE_BUILD_TYPE:STRING=Release \
       -DAMRWIND_HOME:STRING=${TOP_MOD}/amr-wind \
       -DERF_HOME:STRING=${TOP_MOD}/ERF \
@@ -130,8 +94,8 @@ cmake -DCMAKE_INSTALL_PREFIX:PATH=${TOP}/erf-amrwind-driver-install \
 cd ${TOP}/erf-amrwind-driver-build
 make -j16
 make install
-cd ${TOP}/erf-amrwind-driver-install/bin
-cp ${TOP}/erf-amrwind-driver/Exec/CouetteFlow/input* .
-./erf_mb_couette inputs_amrex
+#cd ${TOP}/erf-amrwind-driver-install/bin
+#cp ${TOP}/erf-amrwind-driver/Exec/CouetteFlow/input* .
+#./erf_mb_couette inputs_amrex
 
 cd ${TOP}
